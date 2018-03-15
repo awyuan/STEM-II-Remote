71628ee5d292af511af3b897d15a7e2d48f27ead
Andrew Yuan <36613780+awyuan@users.noreply.github.com>
2018-03-15 11:41:03 -0400

    Update Universal_Remote.ino

--- Universal_Remote.ino
+++ Universal_Remote.ino
@@ -1,25 +1,62 @@
-//Modified the starting code of a button from http://www.instructables.com/id/How-to-control-your-TV-with-an-Arduino/
-int IRledPin =  13;    // LED connected to digital pin 13
-int delayNum[]={1660,1640}; //These are just testing values 
-int pulseIRNum[]={580,560}; //These are just tesing values 
+/Modified the starting code of a button from http://www.instructables.com/id/How-to-control-your-TV-with-an-Arduino/
+//Tutorials and examples from https://www.arduino.cc/en/Tutorial/Button were used to help with the code 
+int IRledPin =  13;    // LED connected to digital pin 13 
+int buttonPin=2;      //Button connected to pin 2 
+int delayNum[]={790,1680,740}; //initializes the array of the delay numbers --> these numbers are currently place holders 
+int pulseNum[]={450,760,940}; //initializes the array of the pulse numbers --> these numbers are currently place holders 
+
+
+int i=0;             //initializes the counter i to 0 
+int buttonState=0;   //initializes the button reader variable 
  
 // The setup() method runs once, when the sketch starts
  
 void setup()   {                
   // initialize the IR digital pin as an output:
-  pinMode(IRledPin, OUTPUT);      
- 
+  pinMode(IRledPin, OUTPUT); 
+  pinMode(buttonPin, INPUT);
   Serial.begin(9600);
 }
  
 void loop()                     
 {
+buttonState=digitalRead(buttonPin);
 
- //Also if the void loop does not keep going then at the end there should be an if (i==sizeof(delayNum)-1) i=0 so that it goes back to the original 
- //The variable i should change when the button is pressed but I don't know how to make this happen 
-  for(int i=0; i<sizeof(delayNum); i++){
+if(buttonState==HIGH){
+if(i<sizeof(delayNum)-1){
   delayMicroseconds(delayNum[i]); //Time off (Left Column on serial monitor)
-  pulseIR(pulseIRNum[i]);           //Time on  (Right Column on serial monitor)
-  
+  pulseIR(pulseNum[i]);           //Time on  (Right Column on serial monitor)
+    delay(10000);  // wait twenty seconds (20 seconds * 1000 milliseconds)
+    i++;
+}
+ 
+ else{ 
+  delayMicroseconds(delayNum[i]); //Time off (Left Column on serial monitor)
+  pulseIR(pulseNum[i]);           //Time on  (Right Column on serial monitor)
   delay(10000);  // wait twenty seconds (20 seconds * 1000 milliseconds)
+  i=0; 
+ }
+}
   }
+
+ 
+// This procedure sends a 38KHz pulse to the IRledPin 
+// for a certain # of microseconds. We'll use this whenever we need to send codes
+void pulseIR(long microsecs) {
+  // we'll count down from the number of microseconds we are told to wait
+ 
+  cli();  // this turns off any background interrupts
+ 
+  while (microsecs > 0) {
+    // 38 kHz is about 13 microseconds high and 13 microseconds low
+   digitalWrite(IRledPin, HIGH);  // this takes about 3 microseconds to happen
+   delayMicroseconds(10);         // hang out for 10 microseconds
+   digitalWrite(IRledPin, LOW);   // this also takes about 3 microseconds
+   delayMicroseconds(10);         // hang out for 10 microseconds
+ 
+   // so 26 microseconds altogether
+   microsecs -= 26;
+  }
+ 
+  sei();  // this turns them back on
+}
