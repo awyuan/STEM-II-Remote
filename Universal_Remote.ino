c49b25eeb04125d156381a7503276bd3640e1937
Andrew Yuan <36613780+awyuan@users.noreply.github.com>
2018-04-02 15:33:33 -0400

    Update Universal_Remote.ino

--- Universal_Remote.ino
+++ Universal_Remote.ino
@@ -2,16 +2,17 @@
 //Tutorials and examples from https://www.arduino.cc/en/Tutorial/Button were used to help with the code 
 int IRledPin =  13;    // LED connected to digital pin 13 
 int buttonPin = 3;      //Button connected to pin 3 
-int channel1[]={740,1680,740}; //Channel Example
-int channel2[]={300,10,300,10,300}; //Channel Example 2
-int channel3[]={100,100,100,100,100}; //Channel Example 3
+int channel1[]={740000,1680000,740000}; //Channel Example
+int channel2[]={300000,100000,300000,100000,300000}; //Channel Example 2
+int channel3[]={100000,100000,100000,100000,100000}; //Channel Example 3
+
 
 int i=0;             //initializes the counter i to 0 
 int buttonState;   //initializes the button reader variable 
  
 // The setup() method runs once, when the sketch starts
  
-void setup()   {                
+void setup(){                
   // initialize the IR digital pin as an output:
   pinMode(IRledPin, OUTPUT); 
   pinMode(buttonPin, INPUT);
@@ -22,9 +23,10 @@ void setup()   {
 void loop()                     
 {
 buttonState=digitalRead(buttonPin);
+Serial.println(buttonState);
   if(buttonState==LOW){
-    if(i==1){
-      for(int c = 0; c<sizeof(channel1); i++){
+    if(i==0){
+      for(int c = 0; c<sizeof(channel1); c++){
         if(c%2==0){
           pulseIR(channel1[c]);
         }
@@ -34,8 +36,8 @@ buttonState=digitalRead(buttonPin);
       }
     i++;
     }
-    if(i==2){
-      for(int c = 0; c<sizeof(channel1); i++){
+    if(i==1){
+      for(int c = 0; c<sizeof(channel1); c++){
         if(c%2==1){
           pulseIR(channel2[c]);
         }
@@ -47,7 +49,7 @@ buttonState=digitalRead(buttonPin);
     }
     
     else{ 
-       for(int c = 0; c<sizeof(channel1); i++){
+       for(int c = 0; c<sizeof(channel1); c++){
         if(c%2==1){
           pulseIR(channel3[c]);
         }
@@ -81,3 +83,4 @@ void pulseIR(long microsecs) {
  
   sei();  // this turns them back on
 }
+
